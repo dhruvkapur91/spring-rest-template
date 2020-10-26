d6c92b10f34f592a2143fd51a12b83fcadffdd0a
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-10-26 13:40:02 +0530

    take isbn as a path variable

--- src/main/java/com/example/bootcamp/BootcampApplication.java
+++ src/main/java/com/example/bootcamp/BootcampApplication.java
@@ -3,13 +3,14 @@ package com.example.bootcamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class IsbnRestController {
-    @GetMapping("/books/")
-    public String lookupBookByIsbn() {
-        return "Book not found";
+    @GetMapping("/books/{isbn}")
+    public String lookupBookByIsbn(@PathVariable("isbn") String isbn) {
+        return "Book not found for the " + isbn;
     }
 }
 
